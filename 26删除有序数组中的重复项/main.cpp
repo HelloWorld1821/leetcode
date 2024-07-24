91b737d5a47e375737b4a7ec165ffb62f841baec
HelloWorld1821 <msmlearnery@gmail.com>
2024-07-25 00:12:05 +0800

    上传26删除有序数组中的重复项代码

---
+++ 26删除有序数组中的重复项/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int len = nums.size();
+        if (len < 2) return len;
+        int k = 0;
+        int mmax = 1e4+5;
+        for (int i = 1; i < len; i++) {
+            if (nums[i] == nums[i - 1]) {
+                k++;
+                nums[i - 1] = mmax;
+            }
+        }
+        if (k == 0) return len;
+        int i = 0, j = 0;
+        while(i < len - k && j < len) {
+            while(i < len - k && nums[i] != mmax)
+            {
+                i++;
+                j = i;
+            }
+            while(j < len && nums[j] == mmax) j++;
+            swap(nums[i], nums[j]);
+            i++;
+            j++;
+        }
+        return len - k;
+    }
+};
+int main() {
+    Solution s;
+    vector<int> nums = {1, 2, 2};
+    cout << s.removeDuplicates(nums) << endl;
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    return 0;
+}
