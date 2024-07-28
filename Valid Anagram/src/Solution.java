7b24e4027134fb4d08236bbbf0ce06139cd122d4
HelloWorld1821 <msmlearnery@gmail.com>
2024-07-28 23:21:49 +0800

    上传242有效的字母异位词代码

---
+++ Valid Anagram/src/Solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String s1 = "anagram";
+        String s2 = "nagaram";
+        if (s.isAnagram(s1, s2)) {
+            System.out.println("Anagram");
+        }
+    }
+}
