115b199442609ed4deab4551bb8ce00963965ec7
HelloWorld1821 <msmlearnery@gmail.com>
2024-07-30 18:07:48 +0800

    上传101对称二叉树代码

---
+++ Symmetric Tree/src/Solution.java
@@ -0,0 +1,56 @@
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (root.left == null && root.right == null) {
+            return true;
+        }
+        if (root.left == null || root.right == null) {
+            return false;
+        }
+        TreeNode right = invertTree(root.right);
+        return isSameTree(root.left, right);
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+        Solution s = new Solution();
+        if (s.isSymmetric(root)) {
+            System.out.println("Symmetric");
+        }
+        else {
+            System.out.println("Not Symmetric");
+        }
+    }
+}
