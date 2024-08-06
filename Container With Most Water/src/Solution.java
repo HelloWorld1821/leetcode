1a16fb0f699aa0545bb38bb3e87ab03fd5e2ab7a
HelloWorld1821 <msmlearnery@gmail.com>
2024-08-06 20:15:45 +0800

    上传11盛最多水的容器代码

---
+++ Container With Most Water/src/Solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            int area = (right - left) * Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        Solution s = new Solution();
+        int maxArea = s.maxArea(height);
+        System.out.println(maxArea);
+    }
+}
